d4120352859f4a04e8c34b7fd14ac6982329840a
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-05-18 22:06:16 +0200

    Add `activate_span` as generic method

--- lib/OpenTracing/Role/ScopeManager.pm
+++ lib/OpenTracing/Role/ScopeManager.pm
@@ -28,7 +28,11 @@ our $VERSION = '0.08_001';
 
 use Moo::Role;
 
+use Carp;
+
 use Types::Standard qw/CodeRef/;
+use OpenTracing::Types qw/Scope/;
+
 
 
 =head1 DESCRIPTION
@@ -38,9 +42,6 @@ L<OpenTracing::Interface>.
 
 =cut
 
-
-
-requires 'activate_span';
 # The chosen design is to have only 1 active scope and use callback to change
 # what the 'previous' scope would be when we close a scope.
 #
@@ -57,6 +58,27 @@ has _active_scope => (
 
 
 
+sub activate_span {
+    my $self = shift;
+    my $span = shift or croak "Missing OpenTracing Span";
+    
+    my $options = { @_ };
+    
+    my $finish_span_on_close = 
+        exists( $options->{ finish_span_on_close } ) ?
+            !! delete $options->{ finish_span_on_close }
+            : !undef
+    ; # use 'truthness' of param if provided, or set to 'true' otherwise
+    
+    my $scope = $self->scope_builder( $span,
+        finish_span_on_close => $finish_span_on_close,
+        %$options,
+    );
+    
+    $self->set_active_scope( $scope );
+    
+    return $scope
+}
 
 
 
